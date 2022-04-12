590cdf2f5923ab76c5ccc654fa1f8244dad3107c
gitthathonor <jeong.suyoung92@gmail.com>
2022-04-12 15:30:16 +0900

    x만큼 간격이 있는 n개의 숫자배열

--- ProgrammerCodeTest/src/low/sec01/Between.java
+++ ProgrammerCodeTest/src/low/sec01/Between.java
@@ -1,7 +1,34 @@
 package low.sec01;
 
+import java.util.ArrayList;
+
 public class Between {
+
+	public static void main(String[] args) {
+
+		Solution s = new Solution();
+		long[] a = s.solution(12,5);
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
+
+class Solution {
 	
-	
-	
+	public long[] solution(int x, int n) {
+		long[] answer = {};
+		
+		if(( 1<=n && n<=1000 ) && (- 10000000<=x && x<=10000000 )) {
+			answer = new long[n];
+			for(int i=0; i<n; i++) {
+				answer[i] = (i + 1) * x;
+			}
+		}
+		
+		return answer;
+	}
 }
