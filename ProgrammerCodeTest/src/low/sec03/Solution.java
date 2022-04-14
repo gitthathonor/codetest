98f5bda8626271f852d3a559a2cecdd08b9549ac
gitthathonor <jeong.suyoung92@gmail.com>
2022-04-14 22:22:56 +0900

    프로그래머스 코딩 예제 - 하샤드 수 풀이중

---
+++ ProgrammerCodeTest/src/low/sec03/Solution.java
@@ -0,0 +1,43 @@
+package low.sec03;
+
+public class Solution {
+	// x는 양의 정수
+	// 하샤드 수는 x의 자릿수의 합이 x로 나누어 떨어지는 수 == 나머지가 0인 것
+	// x는 1이상 10000이하
+	// 자릿수는 1~5까지 나옴
+	// 1~9
+	// 10~99
+	// 100~999
+	// 1000~9999
+	// 10000
+	// 자릿수의 합은 1~36 사이의 범위
+	// 
+
+	public boolean solution(int x) {
+		boolean answer = true;
+		
+		int arrNum = 0;
+		
+		switch (arrNum) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			
+			break;
+		default:
+			break;
+		}
+
+		return answer;
+	}
+}
