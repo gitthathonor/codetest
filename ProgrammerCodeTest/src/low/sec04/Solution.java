3c1bd744aefbfa57cb34baa1cc7531c793a9b055
gitthathonor <jeong.suyoung92@gmail.com>
2022-04-15 21:54:15 +0900

    프로그래머스 예제 - 자릿수 더하기

---
+++ ProgrammerCodeTest/src/low/sec04/Solution.java
@@ -0,0 +1,21 @@
+package low.sec04;
+
+import java.util.*;
+
+/*자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution함수를
+만들어 주세요.
+ex) N = 123 이면 1 + 2 + 3 = 6을 return 하면 됩니다.*/
+
+
+
+public class Solution {
+	 public int solution(int n) {
+	        int answer = 0;
+
+	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	        System.out.println("Hello Java");
+
+	        return answer;
+	    }
+	
+}
