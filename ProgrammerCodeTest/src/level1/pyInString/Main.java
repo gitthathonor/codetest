4a43df17ec81a55e848e6f68dca907d4a883ef91
gitthathonor <jeong.suyoung92@gmail.com>
2022-07-17 22:19:44 +0900

    레벨1 - 문자열 내 p와 y의 갯수 풀이중

---
+++ ProgrammerCodeTest/src/level1/pyInString/Main.java
@@ -0,0 +1,20 @@
+package level1.pyInString;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Solution {
+	boolean solution(String s) {
+        boolean answer = true;
+
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
