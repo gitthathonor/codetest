93057faf537c8e90032876f856aeee4e82f2ece5
gitthathonor <jeong.suyoung92@gmail.com>
2022-07-24 14:50:44 +0900

    레벨1 - 문자열 내 p와 y의 갯수

--- ProgrammerCodeTest/src/level1/pyInString/Main.java
+++ ProgrammerCodeTest/src/level1/pyInString/Main.java
@@ -3,18 +3,33 @@ package level1.pyInString;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Solution sol = new Solution();
+		String a = "Pyy";
+		boolean answer = sol.solution(a);
+		System.out.println(answer);
+		 
 	}
 
 }
 
 class Solution {
 	boolean solution(String s) {
-        boolean answer = true;
+		boolean answer = false;
+        int pNum = 0; //p의 갯수가 담길 변수
+        int yNum = 0; //y의 갯수가 담길 변수
 
-        System.out.println("Hello Java");
-
+        for(int i=0; i<s.length(); i++) {
+        	if((s.charAt(i)+"").equals("p")  || (s.charAt(i)+"").equals("P")) {
+        		pNum += 1;
+        	} else if((s.charAt(i)+"").equals("y") || (s.charAt(i)+"").equals("Y")) {
+        		yNum += 1;
+        	}
+        }
+        
+        if(pNum == yNum) {
+        	answer = true;
+        }
+    
         return answer;
     }
 }
